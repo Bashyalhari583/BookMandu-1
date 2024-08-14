57e9f77e71c7c9d70aaeea50b7ca9d1ece635001
Debendra Aryal <84509099+debendra-aryal@users.noreply.github.com>
2024-08-14 21:11:09 +0545

    latest change

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+
 function openOverlay(overlayId) {
-    document.getElementById(overlayId).style.display = 'flex';
+    const overlay = document.getElementById(overlayId);
+    overlay.style.display = 'flex';
+
+    // Add an event listener to close the overlay when clicking outside of it
+    window.addEventListener('click', function (event) {
+        if (event.target === overlay) {
+            closeOverlay(overlayId);
+        }
+    });
 }
 
 function closeOverlay(overlayId) {
     document.getElementById(overlayId).style.display = 'none';
 }
 
+// Switch between overlays
 function switchOverlay(closeOverlayId, openOverlayId) {
     closeOverlay(closeOverlayId);
     openOverlay(openOverlayId);
 }
+
+
+
+
+
+
+
